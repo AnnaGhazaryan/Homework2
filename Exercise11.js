85bf0f1ee26ced134b507a86e3774b5ddb465591
AnnaGhazaryan <58561909+AnnaGhazaryan@users.noreply.github.com>
2019-12-13 22:30:38 +0400

    Create Exercise11.js

---
+++ Exercise11.js
@@ -0,0 +1,9 @@
+//Exercise 11
+console.log("______________Exercise 11")
+var k = 12;
+var last;
+var first;
+k = k.toString();
+last = k.slice(k.length-1,k.length);
+first =k.slice(0,1);
+console.log(last+k.slice(1,k.length-1)+first);
