3df7ee4006d32adac6b294051b6195ca9f4dedd4
AnnaGhazaryan <58561909+AnnaGhazaryan@users.noreply.github.com>
2019-12-13 22:29:27 +0400

    Create Exercise9.js

---
+++ Exercise9.js
@@ -0,0 +1,13 @@
+//Exercise 9
+console.log("______________Exercise 9");
+var n = +prompt ();
+var n = 8672;
+var i = 0;
+var j = 0;
+if ((n % 2 === 0) && !Math.floor(n/10)) {
+  i += 1;
+}
+
+if ((n % 3 === 0) && (n % 10 === 1)) {
+  j += 1;
+}
